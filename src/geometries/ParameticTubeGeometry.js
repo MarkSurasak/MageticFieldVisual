39efee0c27decde0a1ad9212142bbc02530e86ae
MarkSurasak <61034796+MarkSurasak@users.noreply.github.com>
2022-08-13 13:21:49 +0700

    Update ParameticTubeGeometry.js

--- src/geometries/ParameticTubeGeometry.js
+++ src/geometries/ParameticTubeGeometry.js
@@ -1,11 +1,11 @@
 import { TubeGeometry, CatmullRomCurve3, Vector3 } from 'three'
-import '../math/Utils'
+import * as utils from '../math/Utils'
 
 class ParameticTubeGeometry extends TubeGeometry {
 
     Constructor (curve, tubularSegment = 64, radius = 1, radialSegment = 8, close = false) {
         const time_span = tf.linspace(0, 1, tubularSegment/4);
-        const points = asVector3(curve.getPointsTensor(time_span))
+        const points = utils.asVector3(curve.getPointsTensor(time_span))
 
         super(new CatmullRomCurve3 (points), tubularSegment, radius, radialSegment, close)
     }
