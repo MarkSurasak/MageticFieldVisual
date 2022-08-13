d175eeb47d9c6de9ebfbb04fd536c4a507e99a90
MarkSurasak <61034796+MarkSurasak@users.noreply.github.com>
2022-08-13 16:45:11 +0700

    Update and rename TensorCurve.js to ParametricCurve.js

---
+++ src/math/ParametricCurve.js
@@ -0,0 +1,42 @@
+import { Curve } from "three"
+
+import * as utils from "./Utils";
+
+import * as tf from "@tensorflow/tfjs-core";
+import "@tensorflow/tfjs-backend-webgl";
+import "@tensorflow/tfjs-core/dist/public/chained_ops/register_all_chained_ops";
+
+class ParametricCurve Curve{
+  
+  //return a tensor of derivatives
+  //param: time_span type = tensor with shape [n,1]
+  //param: every element in time_span must between 0 and 1
+  //return: tensor with shape [3,n]
+  get_points_tensor(time_span) {}
+
+  //return a tensor of derivatives
+  //param: time_span type = tensor with shape [n,1]
+  //param: every element in time_span must between 0 and 1
+  //return: tensor with shape [3,n]
+  get_derivatives_tensor(time_span) {}
+
+  getPoint(t, optionalTaget = new Vector3()) {}
+
+  getDerivative(t, optionalTaget = new Vector3()) {}
+
+  getDerivatives(division = 5) {
+    const time_span = tf.linspace(0,1, division)
+    return utils.asVector(this.get_derivatives_tensor(time_span))
+  }
+
+  getPoints(division = 5) {
+    const time_span = tf.linspace(0,1, division)
+    return utils.asVector(this.get_point_tensor(time_span))
+  }
+
+  getTangent(t, optionalTaget = new Vector3()) {
+    return optionaoTager.copy(this.getDerivative(t).normalize())
+  }
+}
+
+export { ParametricCurve };
