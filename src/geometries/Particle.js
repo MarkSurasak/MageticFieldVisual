2347af5bd32849dd5bece1902fa37d546b344db9
MarkSurasak <61034796+MarkSurasak@users.noreply.github.com>
2022-08-13 13:20:41 +0700

    Update Particle.js

--- src/geometries/Particle.js
+++ src/geometries/Particle.js
@@ -1,11 +1,11 @@
 import { BufferGeometry, Point, Vector3, Clock } from 'three'
-import { Util } from '../math/Util'
+import * as utils from '../math/Utils'
 
 class Particle extends Point {
 
     Constructor (clock, initial_points, vectorfield, material) {
         const geometry = new BufferGeometry()
-        geometry.setFromPoint(Util.asVectors3(initial_points)))
+        geometry.setFromPoint(utils.asVectors3(initial_points)))
 
         super(geometry, material)
 
@@ -16,7 +16,10 @@ class Particle extends Point {
     }
 
     update() {
-        this.point_tensor = this.integateMedthod(this.vectorfield, this.clock.getElapsedTime(), this.points_tensor, this.clock.getDeltaTime())
+        const next = this.integateMedthod(this.vectorfield, this.clock.getElapsedTime(), this.points_tensor, this.clock.getDeltaTime())
+        
+        this.geometry.vertices = utils.asVertices(next)
+        this.points_tensor = next
     }
 }
 
